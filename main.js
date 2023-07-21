cb74eb068ecfd532babac75745e3755f3907bc1d
paytonwu90 <sw9008k@gmail.com>
2023-07-21 21:15:47 +0800

    首頁

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 import './assets/scss/all.scss';
 
-console.log("Hello world!");
+document.addEventListener('click', function(e) {
+  const target = e.target;
+  if (target.matches('a[href="#"]') || target.parentElement.matches('a[href="#"]')) {
+    e.preventDefault();
+  }
+});
+
+document.addEventListener('submit', function(e) {
+  e.preventDefault();
+});
